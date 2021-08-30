d9a65934201a1499ce14ee8faaf94056d48e929d
Patrick Chacón <patrickchacon77@hotmail.com>
2021-08-29 22:04:56 -0500

    creación del modo dark

--- public/js/main.js
+++ public/js/main.js
@@ -20,5 +20,33 @@ const typed = new Typed('.typed', {
 });
 
 
-// scrool
+// boton dark 
+const btnSwitch = document.querySelector('.switch');
 
+btnSwitch.addEventListener('click', () =>{
+    document.body.classList.toggle('dark'); //agregar clase de dark
+    btnSwitch.classList.toggle('active');//cambia estado del boton
+
+
+    //guardamos el modo en localstorage
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    }else{
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+
+
+
+
+
+
+//comprabación de localstoragge variable dark
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark'); //agregar clase de dark
+    btnSwitch.classList.add('active');//cambia estado del boton
+}else{
+    document.body.classList.remove('dark'); //quitar clase de dark
+    btnSwitch.classList.remove('active');//cambia estado del boton
+}
